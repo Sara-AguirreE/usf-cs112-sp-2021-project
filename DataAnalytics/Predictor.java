9df59307964d0dd91352f55ef3492a486074695b
Sara Aguirre Espinoza <saraaguirreespinoza@saras-air.attlocal.net>
2021-03-30 23:16:16 -0700

    added a new Predictor Java file and code

---
+++ DataAnalytics/Predictor.java
@@ -0,0 +1,15 @@
+public class Predictor {
+	
+	public abstract ReadData(DataPoint[] data){
+		
+	}
+	public abstract Double getAccuracy(ArrayList<DataPoint> data) {
+		
+	}
+	
+	publiv abstract Double get Precision(ArrayList<DataPoint> data) {
+		
+	}
+	public static void main(String[]args) {
+		
+	}
